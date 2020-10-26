58663653fefb2d8c77f792568bbb4d7fe820d5e1
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-10-26 18:45:29 +0530

    NS1.2

--- src/components/App.js
+++ src/components/App.js
@@ -5,45 +5,64 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
+  const reset = () => {
+    setRenderBall(false);
+setX(0); setY(0);
+let newobj = {
+  left: `0px`,
+  top: `0px`
+};
+setBallPosition(newobj);
+  };
   const start = () => {
     setRenderBall(true);
   };
-  const renderChoice = () => {
+  const renderChoice = () => {};
+
+  const keyEventHandler = (event) => {
+    let left = x,
+      top = y;
+    console.log(event.keyCode);
+    if (event.keyCode === 37) {
+      left = x - 5;
+      setX(left);
+    } else if (event.keyCode === 38) {
+      top = y - 5;
+      setY(top);
+    } else if (event.keyCode === 39) {
+      left = x + 5;
+      setX(left);
+    } else if (event.keyCode === 40) {
+      top = y + 5;
+      setY(top);
+    }else{return;}
+    let newobj = {
+      left: `${left}px`,
+      top: `${top}px`
+    };
+    setBallPosition(newobj);
   };
 
-const keyEventHandler = (event) =>{
-if(event.keyCode === 37){
-
-}
-else if(event.keyCode === 38){
-
-}
-else if(event.keyCode === 39){
-
-}
-else if(event.keyCode === 40){
-
-}
-
-}
-
-React.useEffect(()=>{
-  document.addEventListener('keydown', keyEventHandler);
-}
-)
+  React.useEffect(() => {
+    document.addEventListener("keydown", keyEventHandler);
+    return () => {
+      document.removeEventListener("keydown", keyEventHandler);
+    };
+  });
 
   return (
     <div className="playground">
-     {  renderBall ? <div className="ball" style={ballPosition}></div>
-     :  <button onClick={start} className="start">
+      {renderBall ? (
+        <div className="ball" style={ballPosition}></div>
+      ) : (
+        <button onClick={start} className="start">
           Start
         </button>
-      }
+      )}
       <button onClick={reset} className="reset">
         Reset
       </button>
